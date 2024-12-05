b26d6621b6cc1f9ad59e07d60539628032c4be5f
alphaDevloper <abdullahdanyal1234@gmail.com>
2024-12-05 18:38:30 +0500

    initial changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
     ],
     theme: {
         container: {
-            center: "true",
+            center: true,
             padding: {
                 DEFAULT: "1rem",
                 md: "2rem",
